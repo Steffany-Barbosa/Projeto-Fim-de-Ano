bacff98a692516a4e179abcf31fffaf03b1de934
Steffany-Barbosa <steffany.lvb@gmail.com>
2024-11-22 11:42:34 -0300

    :lipstick: Estilizando

--- Desafio-Final/src/pages/inicio/Inicio.jsx
+++ Desafio-Final/src/pages/inicio/Inicio.jsx
@@ -1,5 +1,9 @@
 
 import s from './inicio.module.scss'
+import community from '../../assets/community.png'
+import reading from '../../assets/reading.png'
+import transform from "../../assets/transform.png";
+import balance from "../../assets/balance.png";
 
 
 export default function Inicio(){
@@ -9,7 +13,39 @@ export default function Inicio(){
           <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
         </section>
 
-         
+        <h2>Por que devo doar</h2>
+
+          <section className={s.cardContainer}>
+            <section className={s.card}>
+              <img src={community} alt="comunidade" className={s.cardImage}/>
+              <section className={s.cardContent}>
+            <p> Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.</p>
+            </section>
+            </section>  
+
+            <section className={s.card}>
+              <img src={reading} alt="lendo" />
+              <section className={s.cardContent}>
+            <p> Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.</p>
+            </section>
+            </section>
+
+            <section className={s.card}>
+              <img src={transform} alt="transformação" />
+              <section className={s.cardContent}>
+            <p> Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.</p>
+            </section>
+            </section>
+
+            <section className={s.card}>
+              <img src={balance} alt="balance" />
+              <section className={s.cardContent}>
+            <p> Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.</p>
+            </section>
+            </section>
+
+            </section>      
+
       </main>
 
        
