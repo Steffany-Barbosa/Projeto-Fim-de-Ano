2f79623aadf8c25d15460c825f955c27e6117c32
Steffany-Barbosa <steffany.lvb@gmail.com>
2024-12-02 10:24:03 -0300

    💄 Fim da estilização

--- Desafio-Final/src/pages/livrosDoados/LivrosDoados.jsx
+++ Desafio-Final/src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,5 +1,21 @@
+import protagonista from '../../assets/protagonista.png'
+import s from './livrosDoados.module.scss'
+
 export default function LivrosDoados(){
     return (
-        <h1>Livros Doados</h1>
+        
+        <section className={s.livrosDoados}>
+            <h2>Livros Doados</h2>
+            <section className={s.containerCards}>
+                <section>
+                    <img src={protagonista} alt="imagem do livro O protagonista" />
+                    <div>
+                    <h3>O Protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                    </div>
+                </section>
+            </section>
+        </section>
     )
 }
